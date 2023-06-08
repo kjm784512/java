875b9144384811831504379b1b6c6d852be0b12e
김지민 <125881468+kjm784512@users.noreply.github.com>
2023-06-08 17:56:16 +0900

    Create _01_Method.java

---
+++ chap_06/_01_Method.java
@@ -0,0 +1,28 @@
+package chap_06;
+
+public class _01_Method {
+
+	// 메소드 클래스내에 있는 함수이다.
+	// 자바에서는 함수는 클래스에 내에서만 존재할 수 있다.
+	// 그래서 자바에 한해서 함수를 메소드라고 부르기로 했다
+	
+	public static void main(String[] args) {
+		System.out.println("메인입니다!!");
+		
+		
+		// 메소드호출
+		System.out.println("메소드 호출 전");
+		hello();
+		hello();
+		hello();
+		System.out.println("메소드 호출 후 ");
+	}
+	
+	
+	// 메소드 정의
+	private static void hello() {
+		System.out.println("Hello World!!");
+
+	}
+
+}
