dca85864defd1729c68d3a6ac9c65cd8f65585d5
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 11:58:32 +0900

    Create S21210_00v1.java

---
+++ S21210/S21210_00v1.java
@@ -0,0 +1,13 @@
+package s21210_00;
+
+public class S21210_00v1 {
+
+	public static void main(String[] arge) {
+		int sum = 0;
+		for(int cnt = 1; cnt < 11; cnt ++) {
+			sum = sum + cnt;
+			System.out.println("cnt :" + cnt + " sum :" + sum);
+		}
+	}
+	
+}
