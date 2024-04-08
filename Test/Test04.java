64bc487fd2f09d0cdc1c68a450391e54a2bc784f
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 12:22:28 +0900

    Create Test04.java

---
+++ Test/Test04.java
@@ -0,0 +1,15 @@
+package s21210_00;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		int cnt =1;
+		int sum =0;
+		
+		while(cnt <=10){
+			sum=sum +cnt;
+			cnt++;
+		}
+		System.out.println("sum: "+sum);
+	}
+}
