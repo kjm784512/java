22cb57dbafa5afe1cdb972f39c2733c30e95e893
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 12:22:15 +0900

    Create Test03.java

---
+++ Test/Test03.java
@@ -0,0 +1,25 @@
+package s21210_00;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		// 숫자 연산
+		int value = 10 + 2 +8;
+		System.out.println("value: " + value);
+		
+		
+		// 결합
+		String str1 = 10 + 2 + "8";
+		System.out.println("str1: " +str1);
+		
+		String str2 = 10 + "2" + 8;
+		System.out.println("str2: " + str2);
+		
+		String str3 = "10" + 2 + 8;
+		System.out.println("str3: " + str3);
+		
+		String str4 = "10" + (2 + 8);
+		System.out.println("str4: " + str4);
+		
+	}
+}
