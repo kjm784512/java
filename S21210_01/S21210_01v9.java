b273a617bc5e49f88e9aa61d89ea7b2bb709689b
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 12:06:17 +0900

    Create S21210_01v9.java

---
+++ S21210_01/S21210_01v9.java
@@ -0,0 +1,18 @@
+package s21210_00;
+
+public class S21210_01v9 {
+
+	public static void main(String[] args) {
+		int intx =1;
+		int inty =1;
+		int intXnum = ++ intx +10;
+		int intYnum = inty++ +10;
+		
+		System.out.println("++intXnum:" + intXnum+"intX:"+intx );
+		System.out.println("intYnum++:" + intYnum+"intY:"+inty );
+		
+		byte byte1 =1;
+		byte byte2 =1;
+		//byte byte3 =byte1 + byte2
+	}
+}
