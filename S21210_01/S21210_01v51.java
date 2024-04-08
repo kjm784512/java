643de759e55f3e371517ed21e0f49e7a1885384e
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 12:03:50 +0900

    Create S21210_01v51.java

---
+++ S21210_01/S21210_01v51.java
@@ -0,0 +1,20 @@
+package s21210_00;
+
+import java.io.IOException;
+
+public class S21210_01v51 {
+
+	public static void main(String[] args) throws IOException {
+		
+		int keyCode;
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+	}
+}
