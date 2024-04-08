6e1856784fd868835146c37d704070f94eeac0a8
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 12:22:56 +0900

    Create TestFormat01.java

---
+++ Test/TestFormat01.java
@@ -0,0 +1,13 @@
+package s21210_00;
+
+public class TestFormat01 {
+
+	public static void main(String[] args) {
+		System.out.printf("이름: %s", "우리들  ");
+		System.out.printf("학번 %d\n", 21210);
+
+		System.out.printf("이름: ", "우리들  ");
+		//System.out.printf("학번 %d\n", "전호번호:  ",010-3502-6815);
+		
+	}
+}
