1906abed77ea5415908ff4cc00d379bb08fe5487
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-08 12:04:06 +0900

    Create S21210_01v5Test.java

---
+++ S21210_01/S21210_01v5Test.java
@@ -0,0 +1,12 @@
+package s21210_00;
+
+import java.io.IOException;
+
+public class S21210_01v5Test {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("키코드입력해라 10자슥아");
+		int keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+	}
+}
