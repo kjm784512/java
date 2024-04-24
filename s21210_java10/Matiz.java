e59e5245b88255701e2db10538cbe9732fb02677
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-04-24 11:37:34 +0900

    Create Matiz.java

---
+++ s21210_java10/Matiz.java
@@ -0,0 +1,11 @@
+package s21210_java11; 
+// 클래스
+
+import s21210_java10.Dog;
+
+public class Matiz  extends Dog{
+   public void a() {
+      System.out.println("5Matiz_패키지를 넘나들며 멍과 바다에 갑니다 ^^");
+   }
+
+}
