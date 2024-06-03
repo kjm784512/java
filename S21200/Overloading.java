8b958c451960f8381934fa50a1bb7d3637fea95d
김지민 <125881468+kjm784512@users.noreply.github.com>
2024-06-03 12:13:29 +0900

    Create Overloading.java

---
+++ S21200/Overloading.java
@@ -0,0 +1,13 @@
+package s21210_java30;
+
+public class Overloading {
+
+	public static void main(String[] args) {
+		OverLoad ol = new OverLoad();
+		ol.cal();
+		ol.cal(3, 4);
+		ol.cal(3);
+		ol.cal(3.14);
+		
+	}
+}
